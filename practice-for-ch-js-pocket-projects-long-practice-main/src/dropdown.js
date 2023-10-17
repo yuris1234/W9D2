064ce1f28de77de2fe9ea512025dac317873972f
Jason Jun <junjason@umich.edu>
2023-10-17 16:07:39 -0400

    working on phase 2

--- practice-for-ch-js-pocket-projects-long-practice-main/src/dropdown.js
+++ practice-for-ch-js-pocket-projects-long-practice-main/src/dropdown.js
@@ -8,3 +8,27 @@ const dogs =
   "Labrador Retriever": "https://www.akc.org/dog-breeds/labrador-retriever/",
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
+
+function dogLinkCreator() {
+  let dogsLink = [];
+  for(key in dogs) {
+    const a = document.createElement("a");
+    a.href = dogs[key];
+    a.innerHTML = key;
+    const li = document.createElement("li");
+    li.className = "dog-link";
+    li.append(a);
+    dogsLink.push(li);
+  }
+  return dogsLink;
+}
+
+function attachDogLinks() {
+  let dogLinks = dogLinkCreator();
+  let ul = document.querySelector(".drop-down-dog-list");
+  dogLinks.forEach((el) => {
+    ul.appendChild(el);
+  });
+}
+
+attachDogLinks();
