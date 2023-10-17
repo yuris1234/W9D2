5f3169922c771d11c3e4fbedc0c85acf79add781
Jason Jun <junjason@umich.edu>
2023-10-17 15:05:40 -0400

    done with phase 0

--- practice-for-ch-js-pocket-projects-long-practice-main/src/warmup.js
+++ practice-for-ch-js-pocket-projects-long-practice-main/src/warmup.js
@@ -1,6 +1,9 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
+    const p = document.createElement("p");
+    p.innerText = string;
+    htmlElement.append(p);
 };
 
 htmlGenerator('Party Time.', partyHeader);
