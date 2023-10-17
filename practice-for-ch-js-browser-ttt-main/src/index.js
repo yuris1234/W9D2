6fd66dc1fa6d71f676cbc8eca1dd62d0d3676269
yuris <yls4@cornell.edu>
2023-10-17 14:05:32 -0400

    got tic tac toe board working

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,12 @@
-Add your import statements for View and Game here
+import View from "./ttt-view.js";
+import Game from "../ttt_node/game.js";
+
+window.Game = Game;
+window.View = View;
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const game = new Game();
+  const gridDiv = document.querySelector(".ttt");
+  const view = new View(game, gridDiv);
+  
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,11 +1,28 @@
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
   }
   
   setupBoard() {
+    const ul = document.createElement("ul");
+    ul.dataset.columns = 3;
+    ul.dataset.rows = 3;
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const li = document.createElement("li");
+        li.dataset.column = j;
+        li.dataset.row = i;
+        ul.appendChild(li);
+      }
+    }
+    this.el.append(ul);
   }
   
   handleClick(e) {
+    
   }
 
   makeMove(square) {
